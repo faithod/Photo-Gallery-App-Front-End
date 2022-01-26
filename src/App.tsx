3ca9eda01e6eebcde9a02d4f4c94828c1e667cf5
Faith <faithodularu@hotmail.com>
2022-01-26 09:33:55 +0000

    imported from react-router-dom, imported components Home + UsersProfile and set up 2 routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import PhotoGallery from "./components/PhotoGallery";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/routes/Home";
+import UsersProfile from "./components/routes/UsersProfile";
 
 function App(): JSX.Element {
   return (
     <>
-      <PhotoGallery />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<UsersProfile />} />
+        </Routes>
+      </Router>
     </>
   );
 }
