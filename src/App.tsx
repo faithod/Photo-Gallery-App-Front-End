96ec4258ab0ce43cf77ffd476db0b7b1d3ddcaae
Faith <faithodularu@hotmail.com>
2022-01-26 15:31:29 +0000

    initial set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/routes/Home";
 import UsersProfile from "./components/routes/UsersProfile";
+import CssBaseline from "@mui/material/CssBaseline"; //might remove later
 
 function App(): JSX.Element {
   return (
     <>
+      <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
