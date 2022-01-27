98c12062a678dfd3946bda947cf3e78510342d74
Faith <faithodularu@hotmail.com>
2022-01-27 18:02:31 +0000

    extracted out helper function fetchData.ts

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,16 @@
+//fetches data using baseUrl
+import { IFavourites } from '../interfaces/IFavourites'
+import { IUser } from '../interfaces/IUser';
+import { baseUrl } from '../utils/baseUrl'
+
+export default function fetchData(endpoint: string, 
+    setState: React.Dispatch<React.SetStateAction<IFavourites[] | undefined>
+  > |React.Dispatch<React.SetStateAction<IUser[] | undefined>>) {
+    fetch(baseUrl + endpoint)
+    .then((res) =>res.json()
+    .then((jsonBody) => {
+      setState(jsonBody.data);
+      console.log(jsonBody.data)
+    })
+    );
+  }
