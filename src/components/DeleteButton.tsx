6565ac88604cd22f5f832afa26cfe62d794c72a3
Faith <faithodularu@hotmail.com>
2022-02-05 04:06:06 +0000

    created a delete button very similar to the favourite button

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,4 +1,4 @@
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import axios from "axios";
@@ -6,28 +6,37 @@ import { IFavourites } from "../interfaces/IFavourites";
 import { IGallery } from "../interfaces/IGallery";
 import { IUser } from "../interfaces/IUser";
 import { baseUrl } from "../utils/baseUrl";
+import fetchData from "../utils/fetchData";
 
-export default function FavouriteButton(props: {
+export default function DeleteButton(props: {
   setIsHovering: React.Dispatch<React.SetStateAction<boolean>>;
   user: IUser | undefined;
   image: IGallery | IFavourites;
+  setFavourites:
+    | React.Dispatch<React.SetStateAction<IFavourites[] | undefined>>
+    | undefined;
 }): JSX.Element {
-  const handleAddToFavourites = () => {
-    if (props.user) {
-      axios.post(baseUrl + `/favourites/${props.user.id}`, {
-        photo_id: props.image.id,
-        alt: props.image.alt,
-        url: props.image.url,
-      });
+  const handleDeletePhoto = () => {
+    if (props.user && props.image) {
+      console.log(props.user.id, props.image.photo_id);
+      axios
+        .delete(
+          baseUrl + `/favourites/${props.user.id}/${props.image.photo_id}`
+        )
+        .then(() => {
+          if (props.setFavourites) {
+            fetchData(`/favourites/${props.user?.id}`, props.setFavourites);
+          }
+        });
     }
   };
 
   return (
-    <Tooltip title="Add to Favourites">
+    <Tooltip title="Delete from Favourites">
       {/* adding sx to add a custom style (to change the icon colour & the colour of the icon when you hover) */}
       <IconButton
         className="btn"
-        aria-label="favourite"
+        aria-label="delete"
         size="small"
         color="default"
         sx={[
@@ -44,9 +53,9 @@ export default function FavouriteButton(props: {
         & so I added onMouseEnter&onMouseLeave here also. */
         onMouseEnter={() => props.setIsHovering(true)}
         onMouseLeave={() => props.setIsHovering(false)}
-        onClick={handleAddToFavourites}
+        onClick={handleDeletePhoto}
       >
-        <FavoriteIcon />
+        <DeleteIcon />
       </IconButton>
     </Tooltip>
   );
