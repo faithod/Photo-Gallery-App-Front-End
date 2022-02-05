2c3f6c33de8804351f699b6b349fe050236d55cd
Faith <faithodularu@hotmail.com>
2022-02-05 05:59:58 +0000

    formatted with prettier

--- src/utils/isFavouriteButtonShown.ts
+++ src/utils/isFavouriteButtonShown.ts
@@ -1,10 +1,14 @@
-import { IUser }from '../interfaces/IUser'
+import { IUser } from "../interfaces/IUser";
 import { Location } from "react-router-dom";
 
-  export function isFavouriteButtonShown(isHovering: boolean, location: Location, user: IUser | undefined): boolean {
-    if (isHovering && location.pathname === "/") {
-        return user ? true : false;
-    } else {
-      return false;
-    }
+export function isFavouriteButtonShown(
+  isHovering: boolean,
+  location: Location,
+  user: IUser | undefined
+): boolean {
+  if (isHovering && location.pathname === "/") {
+    return user ? true : false;
+  } else {
+    return false;
   }
+}
