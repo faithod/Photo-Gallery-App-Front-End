7f7d3a81302954316af7bd02988cc5ca5b7f61ae
Faith <faithodularu@hotmail.com>
2022-02-05 05:39:31 +0000

    formatted with prettier

--- src/utils/isDeleteButtonShown.ts
+++ src/utils/isDeleteButtonShown.ts
@@ -1,7 +1,10 @@
-export function isDeleteButtonShown(isHovering: boolean, location: import("/home/2110-003-fod/Developer/academy/Photo-Gallery-App-Front-End/node_modules/history/index").Location): boolean {
-    if (isHovering && location.pathname === "/profile") {
-      return true;
-    } else {
-      return false;
-    }
+export function isDeleteButtonShown(
+  isHovering: boolean,
+  location: import("/home/2110-003-fod/Developer/academy/Photo-Gallery-App-Front-End/node_modules/history/index").Location
+): boolean {
+  if (isHovering && location.pathname === "/profile") {
+    return true;
+  } else {
+    return false;
   }
+}
--- src/utils/isFavouriteButtonShown.ts
+++ src/utils/isFavouriteButtonShown.ts
@@ -1,9 +1,13 @@
-import { IUser }from '../interfaces/IUser'
+import { IUser } from "../interfaces/IUser";
 
-  export function isFavouriteButtonShown(isHovering: boolean, location: import("/home/2110-003-fod/Developer/academy/Photo-Gallery-App-Front-End/node_modules/history/index").Location, user: IUser | undefined): boolean {
-    if (isHovering && location.pathname === "/") {
-        return user ? true : false;
-    } else {
-      return false;
-    }
+export function isFavouriteButtonShown(
+  isHovering: boolean,
+  location: import("/home/2110-003-fod/Developer/academy/Photo-Gallery-App-Front-End/node_modules/history/index").Location,
+  user: IUser | undefined
+): boolean {
+  if (isHovering && location.pathname === "/") {
+    return user ? true : false;
+  } else {
+    return false;
   }
+}
