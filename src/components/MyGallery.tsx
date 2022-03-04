881cf1311ad8eaf0e6771bce81ea21143b85974f
Faith <faithodularu@hotmail.com>
2022-03-04 11:09:33 +0000

    attempting upload with form data

--- src/components/MyGallery.tsx
+++ src/components/MyGallery.tsx
@@ -10,6 +10,45 @@ const Input = styled("input")({
 });
 
 export default function MyGallery(props: MyGalleryProps): JSX.Element {
+  const [selectedFile, setSelectedFile] = useState<undefined | File>();
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSelectedFile(e.target.files[0]);
+
+      //The FormData interface provides a way to easily construct
+      //a set of key/value pairs representing form fields and their values
+
+      //creating an object of formData
+      const formData = new FormData();
+
+      //update formData
+      if (selectedFile) {
+        formData.append("file", selectedFile);
+      }
+
+      axios.post("api/uploadfile", { user_id: props.user?.id, ...formData });
+
+      console.log(formData);
+
+      // { selectedFile: File; }
+
+      // console.log(JSON.stringify(e.target.files));
+      // const files = Array.from(e.target.files);
+      // console.log(JSON.stringify(files));
+      // const url = URL.createObjectURL(e.target.files[0]);
+      // console.log(url);
+
+      // const formData = new FormData(); // Currently empty
+      // formData.append("username", "Chris");
+      // console.log(formData);
+
+      // files.forEach((file) => {
+      //   formData.append("image", file);
+      // });
+      // console.log(formData);
+    }
+  };
+  //how to access selected file(s) on a change event
   return (
     <>
       {!props.user ? (
@@ -22,6 +61,7 @@ export default function MyGallery(props: MyGalleryProps): JSX.Element {
               accept="image/*"
               id="upload-photo-contained-button"
               type="file"
+              onChange={(e) => handleInputChange(e)}
             />
             <Fab color="primary" aria-label="add" component="span">
               <AddIcon />
